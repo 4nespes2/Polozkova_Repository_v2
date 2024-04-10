6ecf9cb24d4fdf05df9798030e590e6655c38137
4nespes2 <katena-polozkova@mail.ru>
2024-04-10 09:57:46 +0300

    change

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int main(int argc, char **argv)
+
 {
  char name[255];
  printf("Enter your name: ");
@@ -10,3 +11,4 @@ int main(int argc, char **argv)
  printf("Hello %s!\n", name);
  return 0;
 }
+
